7254fcd574c100dc09336fb0d8cb2d205a1215e9
nuhptr <nugrahaadi733@gmail.com>
2024-06-08 11:15:43 +0700

    feat: :art: layout update code

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,21 +3,44 @@ import { Nunito } from "next/font/google"
 
 import "./globals.css"
 
+import { ToasterProvider } from "@/app/providers/ToasterProvider"
+
 import { Navbar } from "@/app/components/navbar/Navbar"
+import { RegisterModal } from "@/app/components/modals/RegisterModal"
+import { LoginModal } from "@/app/components/modals/LoginModal"
+import { SessionProvider } from "next-auth/react"
+
+import { currentUser } from "./hooks/useCurrentuser"
+import { getUserByEmail } from "./data/user"
 
 const nunito = Nunito({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-   title: "CloudBnB | A better way to rent a place",
+   title: "Cloudbnb | A better way to rent a place",
    description: "CloudBnB is a platform that connects renters with landlords in a seamless way.",
+   // add other seo keywords here
 }
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+type RootLayoutTypes = {
+   children: React.ReactNode
+}
+
+export default async function RootLayout({ children }: Readonly<RootLayoutTypes>) {
+   const user = await currentUser()
+   const getUser = await getUserByEmail(user!.email!)
+
    return (
       <html lang="en">
          <body className={nunito.className}>
-            <Navbar />
-            {children}
+            <SessionProvider>
+               <RegisterModal />
+               <LoginModal />
+               <Navbar currentUser={getUser} />
+               {children}
+
+               {/* Register Toaster Provider */}
+               <ToasterProvider />
+            </SessionProvider>
          </body>
       </html>
    )
